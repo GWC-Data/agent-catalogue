9afbc0d7bab7ee960e247368356ec21b37e9691c
yogesh-gwc <yogeshwaran.s@gwcdata.ai>
2026-01-20 16:19:52 +0530

    Modified the code

--- src/pages/agents/it_resolver/components/ResolversTable.tsx
+++ src/pages/agents/it_resolver/components/ResolversTable.tsx
@@ -120,7 +120,7 @@ export function ResolversTable({ data }: { data: Resolver[] }) {
   return (
     <div className="w-full bg-white rounded-md px-6 py-3 shadow-sm border border-gray-200">
       <div className="flex items-center py-4">
-        <div className="text-lg font-bold">Lead Table</div>
+        <div className="text-lg font-bold">Resolvers Table</div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
